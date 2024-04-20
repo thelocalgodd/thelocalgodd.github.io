181048da1a591ae3a7ccfcce409fac370c9dbe67
thelocalgodd <vkkpemlie@st.knust.edu.gh>
2024-04-20 01:06:28 +0000

    done with base features

---
+++ src/components/SpotifyEmbed.jsx
@@ -0,0 +1,11 @@
+import { Spotify } from "react-spotify-embed";
+
+const SpotifyEmbed = () => {
+  return (
+    <Spotify
+        link="https://open.spotify.com/track/78LQXbgXkQeG0it4vnT7cn?si=3517556916a840af"
+    />
+  )
+}
+
+export default SpotifyEmbed
