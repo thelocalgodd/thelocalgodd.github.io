f9d4f962c8e2a744a287a8cc3cae18ae67b351a4
thelocalgodd <vkkpemlie@st.knust.edu.gh>
2024-04-21 23:44:57 +0000

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://test.thelocalgodd.me'
+  site: 'https://test.thelocalgodd.me',
+  integrations: [
+    tailwind(), 
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      }
+    })
+  ]
 });
