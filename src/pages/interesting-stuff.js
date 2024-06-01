7c9d1f66878712c4fc03463d0c0b0b5ffd3c24b5
thelocalgodd <splashvincentk@outlook.com>
2024-06-01 14:05:29 +0000

    started new post

---
+++ src/pages/interesting-stuff.js
@@ -0,0 +1,16 @@
+const interestingItems = [
+  {
+    title: "Github Education Student Developer Pack",
+    description:
+      "The GitHub Student Developer Pack is all you need to learn how to code. Get your Pack now.",
+    link: "https://education.github.com/pack",
+  },
+  {
+    title: "degreat.co.uk",
+    description:
+      "Portfolio site of one of the top top guys i've come across on Twitter.",
+    link: "https://www.freecodecamp.org/",
+  },
+];
+
+export default interestingItems;
---
+++ src/pages/posts/getting-github-education.md
@@ -0,0 +1,8 @@
+---
+title: "getting github education:"
+pubDate: June 1, 2024
+description: "This article talks about how you can get a free student developer pack through github education."
+author: "thelocalgodd"
+tags: ["free-stuff", "coding", "github education"]
+layout: ../../layouts/BlogLayout.astro
+---
