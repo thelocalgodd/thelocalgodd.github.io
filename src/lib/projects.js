8648de11b03c70c7f86e11527e89f636d619f7cc
thelocalgodd <splashvincentk@outlook.com>
2024-12-04 17:41:52 +0000

    updated project page

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -2,13 +2,13 @@ const projectItems = [
   {
     title: "thelocalgodd.github.io",
     description:
-      "this is my current portfolio website I use to blog and showcase my work.",
+      "this is my current portfolio website which I use to blog and showcase my work.",
     link: "https://thelocalgodd.me",
     tag: "astro, tailwindcss",
   },
   {
     title: "analytics system",
-    description: "a full stack self-hosted analytics system I built.",
+    description: "a full stack self-hosted analytics system.",
     link: "https://github.com/thelocalgodd/analytics-system",
     tag: "react, tailwindcss, node, express, mongodb",
   },
