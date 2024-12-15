b7664a9926f0490e3a22dac3fad334018b8d0657
thelocalgodd <splashvincentk@outlook.com>
2024-12-15 22:36:40 +0000

    updated projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -12,6 +12,44 @@ const projectItems = [
     link: "https://github.com/thelocalgodd/analytics-system",
     tag: "react, tailwindcss, node, express, mongodb",
   },
+  {
+    title: "Believer - Song Promotion Page",
+    description: "a song promotion page for Believer by Cl!Q and Yaw Palmer.",
+    link: "https://believer-pi.vercel.app",
+    tag: "react, tailwindcss, wavesurfer.js",
+  },
+  {
+    title: "isPrime",
+    description: "a simple isPrime package for node.js.",
+    link: "https://github.com/thelocalgodd/isPrime",
+    tag: "node",
+  },
+  {
+    title: "tailwind-auto-script",
+    description:
+      "a bash script to automatically create a react + vite project and add tailwindcss to it.",
+    link: "https://github.com/thelocalgodd/tailwind-auto-script",
+    tag: "bash",
+  },
+  {
+    title: "CS KNUST",
+    description: "a repo collection of assignments in the CS Course at KNUST.",
+    link: "https://github.com/thelocalogdd/csknust",
+    tag: "markdown, java, c++, visual basic, asm, sql",
+  },
+  {
+    title: "thelocalblogg",
+    description: "UNCOMPLETED ~ an anonymous blogging website.",
+    link: "https://github.com/thelocalgodd/thelocalblogg",
+    tag: "react, tailwindcss, node, express, mongodb",
+  },
+  {
+    title: "Automated BTC Price Tracker ~ Telegram",
+    description:
+      "get's the price of btc and sends it to a telegram bot every minute.",
+    link: "https://github.com/thelocalgodd/btc_tg_bot",
+    tag: "node, api",
+  },
 ];
 
 export default projectItems;
